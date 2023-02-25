3e6635f87fbbf15c469eada9e862d5be28962738
alisadiq2003 <alisadiq.personal2003@gmail.com>
2023-02-25 19:03:52 +0500

    notes for day5

---
+++ Day 8: Fundamental Data Type - integer (part 2)/day8file5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+
+    short int var1 = SHRT_MIN;
+    short int var2 = SHRT_MIN;
+
+    printf ("range of short simged integers is form: %d to %d", var1, var2);
+    return 0;
+
+
+

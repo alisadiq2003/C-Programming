1610487ff758e16944a99de862219e5888e1e5a8
alisadiq2003 <alisadiq.personal2003@gmail.com>
2023-02-25 19:26:42 +0500

    practice on integer day 6

---
+++ Day 8: Fundamental Data Type - integer (part 2)/day8file6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+
+int mian()
+{
+
+    short unsigned int var1 = 0;
+    short unsigned int vaer2 = USHRT_MAX;
+
+    printf("range of short unsigned integer is form: %u to %u", var1, var2);
+    return 0;
+}

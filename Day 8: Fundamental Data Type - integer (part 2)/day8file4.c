3ef1614b2fb19c090102aa8eb1d7678331dceb63
alisadiq2003 <alisadiq.personal2003@gmail.com>
2023-02-25 17:57:07 +0500

    practice on integer file 4

---
+++ Day 8: Fundamental Data Type - integer (part 2)/day8file4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+
+    unsigned int var1 = 0;
+    unsigned int var2 = UINT_MAX;
+
+    printf("rang of unsinged integer is from: %u to %u", var1, var2);
+    return 0;
+}

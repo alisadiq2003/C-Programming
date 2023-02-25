9777d762e8a573e282a2630a5dfc256c018be770
alisadiq2003 <alisadiq.personal2003@gmail.com>
2023-02-25 17:43:07 +0500

    practice on integer

---
+++ Day 8: Fundamental Data Type - integer (part 2)/file2day8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+
+    int var1 = INT_MIN;
+    int var = INT_MAX;
+    printf("rang of singed integer is from: %d to %d", var1, var2);
+    return 0;
+}
